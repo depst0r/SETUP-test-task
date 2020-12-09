0c2786034f613455bd573e5a83f74ff6a081e5da
depst0r <depstor90@gmail.com>
2020-12-09 13:09:37 +0300

    'errors-message'

--- src/Components/Errors/Errors.jsx
+++ src/Components/Errors/Errors.jsx
@@ -1,7 +1,18 @@
-import React from 'ract'
+import React from 'react'
 
-export const ErrorsMessage = () => {
-    return<>
-        
-    </>
+export const ErrorsMessage = ({ error }) => {
+    if(error) {
+        switch (error.type) {
+            case 'required':
+                return <p>This is required</p>
+            case 'minLength':
+                return <p>Your full name minium</p>
+            case 'pattern':
+                return <p>Enter a valid email address</p>
+            default:
+                return null
+        }
+    }
+    
+    return null
 }
